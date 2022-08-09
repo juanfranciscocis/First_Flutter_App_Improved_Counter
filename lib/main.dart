8c78205e66622e998db4a95e385a768f86e2fd68
Juan Cisneros <juanfrancistm2011@icloud.com>
2022-08-09 18:58:09 -0500

    Prefer Const

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,25 @@ import 'package:flutter/material.dart';
 
 void main(){
   //DONDE TODO MI CODIGO CORRE DE DART
-  runApp(new MyApp()); //IMPORTA EL PAQUETE PRINCIPAL -> MATERIAL.DART
+  runApp(const MyApp()); //IMPORTA EL PAQUETE PRINCIPAL -> MATERIAL.DART
 
 }
 
 
 class MyApp extends StatelessWidget{
+  const MyApp({Key? key}) : super(key: key); //CONSTRUCTOR DE LA CLASE MyApp
+
   //TODO STATELESS WIDGET RECIBE UN METODO BUILD
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp( //WIDGET PRINCIPAL! LE DECIMOS QUE ES UN MATERIAL DESIGN APP -> COMO ESTO ES ESTATICO ENTONCES USO CONST
       //https://api.flutter.dev/flutter/material/MaterialApp-class.html
 
       home: Center(
         child: Text('Hola Mundo'),
       ),
+
+
 
 
     );
