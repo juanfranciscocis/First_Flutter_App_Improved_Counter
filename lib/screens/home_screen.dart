f6d2b3703c9d5595253f259fcb2896b8b31442e1
Juan Cisneros <juanfrancistm2011@icloud.com>
2022-08-09 21:52:36 -0500

    COLUMNAS Y TAREA (IMPORTANTE PARA CENTRAR WIDGETS)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,11 +10,24 @@ class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     //BuildContext es el arbol de widgets completo
-
-    return const Scaffold( //Lienzo donde pongo widgets
+    return  Scaffold( //Lienzo donde pongo widgets (https://api.flutter.dev/flutter/material/Scaffold-class.html)
       //backgroundColor: Colors.blue,
-      body: Center(
-        child: Text('HomeScreen'),
+
+      //(https://docs.flutter.dev/development/ui/widgets/basics) -> widgets
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+        elevation: 10.0, //Sombra!
+      ), //Requiere un Widget de tipo PrefferedSizeWidget!!!
+      body: Center( //Centra horizontal
+        //child: Text('Clicks Counter'), //SOLO HAY UN CHILD EN LOS WIDGETS
+        child: Column(
+          mainAxisAlignment:MainAxisAlignment.center, //Centro vertical
+          crossAxisAlignment:CrossAxisAlignment.center,//Relativo al widget que esta mas grande!!!!, no me sirve
+          children: const <Widget> [
+            Text('Numero de Clicks'),
+            Text('10'),
+          ],
+        )
       ),
     );
 
